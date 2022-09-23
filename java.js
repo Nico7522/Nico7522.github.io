097c4fb928b9c603cc723648b410a6f2f8eecc7d
Nico7522 <nico.daddabbo7100@gmail.com>
2022-09-23 17:12:45 +0200

    fix bug

--- java.js
+++ java.js
@@ -21,7 +21,7 @@ let modeLecture = document.querySelector('#modedelecture');
 let imageDeBase = document.querySelector('#depart');
 let chapselect = document.querySelector('.dropdown-content')
 let chapitre = document.title;
-chapitre = chapitre.toLowerCase();
+chapitre = chapitre.toUpperCase();
 
 let verticale = false;
 
